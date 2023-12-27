e6f2ef559d5268e774c91443fe0292b2191a6dd3
Taweesak <taweesak@Taweesaks-MacBook-Pro.local>
2023-12-27 23:43:36 +0700

    updateLiff12

--- src/App.vue
+++ src/App.vue
@@ -62,37 +62,36 @@ export default {
       window.open("https://liff.line.me/1656824759-dzZxJlQ9", "_blank");
 
       //window.open("line://app/1656824759-dzZxJlQ9");
-      //await liff.init({ liffId: "1656824759-qbyK4305" });
+      await liff.init({ liffId: "1656824759-qbyK4305" });
       //await liff.init({ liffId: "1656824759-qbyK4305" });
 
       //const profile = await liff.getProfile();
 
-      // if (liff.isLoggedIn()) {
+      if (liff.isLoggedIn()) {
+        await liff
+          .sendMessages([
+            {
+              type: "text",
+              // text: `Register/${profile.displayName}`,
+              text: `Register`,
+            },
+            // {
+            //   type: "text",
+            //   text: `userId : ${profile.userId}`,
+            //   //text: `Register`,
+            // },
+          ])
+          .then(() => {
+            window.alert(`Message sent Register `);
+          })
+          .catch((error) => {
+            window.alert("Error sending message: " + error);
+          });
 
-      // await liff
-      //   .sendMessages([
-      //     {
-      //       type: "text",
-      //       // text: `Register/${profile.displayName}`,
-      //       text: `Register`,
-      //     },
-      //     // {
-      //     //   type: "text",
-      //     //   text: `userId : ${profile.userId}`,
-      //     //   //text: `Register`,
-      //     // },
-      //   ])
-      //   .then(() => {
-      //     window.alert(`Message sent Register `);
-      //   })
-      //   .catch((error) => {
-      //     window.alert("Error sending message: " + error);
-      //   });
-
-      //
-      // } else {
-      //   liff.login();
-      // }
+        //
+      } else {
+        liff.login();
+      }
     },
   },
 };
