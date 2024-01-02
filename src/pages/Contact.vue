1743b7843aa8cbbb993148ff77734ed395836e19
Taweesak <taweesak@Taweesaks-MacBook-Pro.local>
2024-01-02 09:47:22 +0700

    update-0101-031

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -1,30 +1,27 @@
 <template>
-  <h1>This is About Page</h1>
-  <div id="formRegister">
-    <form id="statusform" v-on:submit="addProduct">
-      <input type="text" v-model="name" placeholder="Product Name" />
-      <input type="number" v-model="price" placeholder="Price" />
-      <button type="submit">Add</button>
-    </form>
-
-    <router-view></router-view>
+  <h1>This is Contact Page</h1>
+  <div id="contact">
+    <!-- <a href="https://vue-weba-crossdomain.onrender.com/contact"> -->
+    <a href="https://line.me/ti/p/@985lgfum">
+      <button>LINE @</button>
+    </a>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      name: "",
-      price: "",
+      // name: "",
+      // price: "",
     };
   },
   methods: {
-    addProduct(e) {
-      e.preventDefault(); // Prevent page from reloading.
-      console.log("data===> ", this.name, this.price);
-      // save data to google sheet
-      // function req res
-    },
+    // addProduct(e) {
+    //   e.preventDefault(); // Prevent page from reloading.
+    //   console.log("data===> ", this.name, this.price);
+    //   // save data to google sheet
+    //   // function req res
+    // },
   },
 };
 </script>
